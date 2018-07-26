b349971e3c5018a162de6dc9415cee5cb4e4bd96
sans0987 <fairyfaye0226@gmail.com>
2018-07-26 20:53:32 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,6 +1,8 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 
-def mothers_day
-  puts "Happy Mother's Day, Mom!"
+def mothers_day{name}
+  puts "Happy Mother's Day, #{name}!"
 end 
-mothers_day
+mothers_day{sis}.capitilize
+mothers_day{grandma}.capitilize
+mothers_day{mom}.capitilize
